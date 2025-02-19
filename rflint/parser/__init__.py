8f5fb11bec99e238fbfee7981505a2e38b7a2b15
rflinter <pieplu@groove.nu>
2025-02-19 18:20:19 +0100

    fix: flake8

--- rflint/parser/__init__.py
+++ rflint/parser/__init__.py
@@ -1,4 +1,2 @@
-from .parser import (SuiteFolder, ResourceFile, SuiteFile, RobotFactory,
-Testcase, Keyword, Row, Statement, TestcaseTable, KeywordTable)  # noqa: F401
-from .tables import DefaultTable, SettingTable, UnknownTable, VariableTable,
-MetadataTable, RobotTable  # noqa: F401
+from .parser import (SuiteFolder, ResourceFile, SuiteFile, RobotFactory, Testcase, Keyword, Row, Statement, TestcaseTable, KeywordTable)  # noqa: F401
+from .tables import DefaultTable, SettingTable, UnknownTable, VariableTable, MetadataTable, RobotTable  # noqa: F401
