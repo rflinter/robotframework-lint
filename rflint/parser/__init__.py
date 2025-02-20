cff42f3c9cbaf75cd5633474459bfcf425861339
rflinter <pieplu@groove.nu>
2025-02-20 18:22:37 +0100

    flake8

--- rflint/parser/__init__.py
+++ rflint/parser/__init__.py
@@ -9,4 +9,8 @@ from .parser import (
     TestcaseTable,
     KeywordTable
   )  # nofaq: F401
-from .tables import DefaultTable, SettingTable, UnknownTable, VariableTable, MetadataTable, RobotTable  # noqa: F401
+
+from .tables import DefaultTable, SettingTable, UnknownTable, VariableTable, MetadataTable, RobotTable 
+
+__all__ = (SuiteFolder,ResourceFile,SuiteFile,RobotFactory,Testcase,Keyword,Row,TestcaseTable,KeywordTable
+           DefaultTable, SettingTable, UnknownTable, VariableTable, MetadataTable, RobotTable)
